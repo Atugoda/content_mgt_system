b9c0a0c262c44602d54301dac1593d40e56d12fb
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-08-08 14:03:08 +0530

    image size fixed

--- src/Components/Unit.js
+++ src/Components/Unit.js
@@ -6,7 +6,7 @@ export const Unit = ({image, name, city, position}) => {
       <img src={image} alt={name} />
       <h3>{name}</h3>
       <p>
-        <span>{city}</span>
+        <span>{city}</span> <br></br>
         <span>{position}</span>
       </p>
     </div>
