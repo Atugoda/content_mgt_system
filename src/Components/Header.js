3575f7449ad358eafb7d7e790af68b0838a07120
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-08-13 00:19:48 +0530

    145

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,82 @@
-import React from 'react'
+// import React from 'react'
+// import closeMenu from '../image/close-svgrepo-com.svg'
 
-const Header = () => (
-    <header className = "header_container"> 
+// const Header = () => (
+//     <header className = "header_container"> 
+//         {/* <h1>Content <span> Management </span><span>System</span></h1> */}
+//         <h1>Content Management System</h1>
+
+//         <nav>
+//            <ul>
+//             <div className= "header_menuClose" >
+//                 <img src={ closeMenu} alt='close Menu'/>
+//             </div>
+//             <div className="header_menu">
+
+//            <li> Home </li>
+//            <li> About </li>
+//            <li> Contact </li>
+//            <li> Project </li>
+           
+//            </div>
+//            </ul>
+//         </nav>
+//     </header>
+// )
+
+// export default Header
+
+
+import React, { useState } from 'react';
+import closeMenu from '../image/close-svgrepo-com.svg';
+import menu from '../image/menu-svgrepo-com.svg'
+
+const Header = () => {
+ //usestate starting value is to be given
+    const [menuClick, setmenuClick] = useState(false);
+
+
+    return(
+        <header className="header_container">
         {/* <h1>Content <span> Management </span><span>System</span></h1> */}
         <h1>Content Management System</h1>
-        
+    
         <nav>
-           <ul>
+            <img src= {menu} alt= 'menu' style={{
+                width: "30px",
 
-           <li> Home </li>
-           <li> About </li>
-           <li> Contact </li>
-           <li> Project </li>
-           
-           </ul>
+
+            }}/>
+          <ul style={{
+            right : `${menuClick ? "-20px" : "-250px" }`,
+          }}>
+            <div className="header_menuClose">
+              <img
+                src={closeMenu}
+                alt="close Menu"
+                style={{
+                  width: '20px',  /* Adjust width as needed */
+                  height: '20px', /* Adjust height as needed */
+                }}
+              />
+            </div>
+            <div className="header_menu">
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+              <li>Project</li>
+            </div>
+          </ul>
         </nav>
-    </header>
-)
+      </header>
+    );
 
-export default Header
+
+
+}
+
+
+
+ 
+
+export default Header;
