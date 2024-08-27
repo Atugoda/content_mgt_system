89179bf36e995bd7be8ae6df1356b4f05b512993
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-08-27 10:37:32 +0530

    the commit after the roting pages

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@
 // import closeMenu from '../image/close-svgrepo-com.svg'
 
 // const Header = () => (
-//     <header className = "header_container"> 
+//     <header className = "header_container">
 //         {/* <h1>Content <span> Management </span><span>System</span></h1> */}
 //         <h1>Content Management System</h1>
 
@@ -17,7 +17,7 @@
 //            <li> About </li>
 //            <li> Contact </li>
 //            <li> Project </li>
-           
+
 //            </div>
 //            </ul>
 //         </nav>
@@ -26,57 +26,60 @@
 
 // export default Header
 
-
-import React, { useState } from 'react';
-import closeMenu from '../image/close-svgrepo-com.svg';
-import menu from '../image/menu-svgrepo-com.svg'
+import React, { useState } from "react";
+import closeMenu from "../image/close-svgrepo-com.svg";
+import menu from "../image/menu-svgrepo-com.svg";
 
 const Header = () => {
- //usestate starting value is to be given
-    const [menuClick, setmenuClick] = useState(false);
+  //usestate starting value is to be given
+  const [menuClick, setmenuClick] = useState(false);
 
+  return (
+    <header className="header_container">
+      {/* <h1>Content <span> Management </span><span>System</span></h1> */}
+      <h1>Content Management System</h1>
 
-    return(
-        <header className="header_container">
-        {/* <h1>Content <span> Management </span><span>System</span></h1> */}
-        <h1>Content Management System</h1>
+      <nav>
+        <img
+          src={menu}
+          alt="menu"
+          style={{
+            width: "30px",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            setmenuClick(true);
+          }}
+        />
+        <ul
+          style={{
+            right: `${menuClick ? "-20px" : "-4060px"}`,
+          }}
+        >
+          <div className="header_menuClose">
+            <img
+              src={closeMenu}
+              alt="close Menu"
+              style={{
+                width: "20px",
+                height: "20px",
+              }}
+              onClick={() => {
+                setmenuClick(false);
+              }}
+            />
+          </div>
+          <div className="header_menu" style={{ width: "-300px" }}>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Project</li>
+          </div>
+        </ul>
+      </nav>
+    </header>
     
-        <nav>
-            <img src= {menu} alt= 'menu' style={{
-                width: "30px",
-
-
-            }}/>
-          <ul style={{
-            right : `${menuClick ? "-20px" : "-250px" }`,
-          }}>
-            <div className="header_menuClose">
-              <img
-                src={closeMenu}
-                alt="close Menu"
-                style={{
-                  width: '20px',  /* Adjust width as needed */
-                  height: '20px', /* Adjust height as needed */
-                }}
-              />
-            </div>
-            <div className="header_menu">
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-              <li>Project</li>
-            </div>
-          </ul>
-        </nav>
-      </header>
-    );
-
-
-
-}
-
-
-
- 
+  );
+};
 
 export default Header;
